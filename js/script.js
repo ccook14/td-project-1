0f4b6c0f5b56d2ad1eeb4c2db35269bf058fd967
Colin <colin.cook@cox.net>
2018-04-18 15:46:03 -0500

    updated js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('loadQuote').addEventListener("click", () => {
 
+});
+
+var quotes = [
+  {quote : 'The report of my death was an exaggeration.',
+  source : 'Mark Twain'},
+
+  {quote : 'In this world nothing can be said to be certain, except death and taxes.',
+  source : 'Ben Franklin'},
+
+  {quote : 'The hardest thing to understand in the world is the income tax.',
+  source : 'Albert Einstein'},
+
+  {quote : 'In politics stupidity is not a handicap',
+  source : 'Napolean Bonaparte'},
+
+  {quote : 'If you aint first your last',
+  source : 'Ricky Bobby'}
+
+];
+console.log(quotes);
+
+var selectQuote = randomNumber;
+var randomNumber = getRandomQuote();
+
+function getRandomQuote() {
+ randomNumber = (Math.floor(Math.random() * quotes.length));
+ return quotes[randomNumber];
+
+}
+console.log(randomNumber);
